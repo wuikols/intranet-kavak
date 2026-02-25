5a8e31f12f66fbabf98992d65cc82ebf40b9bf0d
wuikols <133246955+wuikols@users.noreply.github.com>
2026-02-25 02:18:30 -0300

    Add files via upload

--- intranet_kavak/config/database.php
+++ intranet_kavak/config/database.php
@@ -4,7 +4,7 @@ class Database
     private $host = "gateway01.us-east-1.prod.aws.tidbcloud.com";
     private $db_name = "github_sample";
     private $username = "2px7t9j3rxyEpdk.root";
-    private $password = "RpUhW1ZeMbB/PuG6";
+    private $password = "FlsaBd0oCzpIcnkv";
     public $conn;
 
     public function getConnection()
@@ -13,9 +13,20 @@ class Database
         try {
             // TiDB Serverless requires port 4000 and STRICT SSL connection
             $options = array(
-                PDO::MYSQL_ATTR_SSL_CA => __DIR__ . '/cacert.pem',
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             );
+
+            // Detect internal Vercel/Linux Certificates paths dynamically
+            if (file_exists('/etc/ssl/certs/ca-certificates.crt')) {
+                $options[PDO::MYSQL_ATTR_SSL_CA] = '/etc/ssl/certs/ca-certificates.crt';
+            }
+            else if (file_exists('/etc/pki/tls/certs/ca-bundle.crt')) {
+                $options[PDO::MYSQL_ATTR_SSL_CA] = '/etc/pki/tls/certs/ca-bundle.crt';
+            }
+            else if (file_exists(__DIR__ . '/cacert.pem')) {
+                $options[PDO::MYSQL_ATTR_SSL_CA] = __DIR__ . '/cacert.pem';
+            }
+
             $this->conn = new PDO("mysql:host=" . $this->host . ";port=4000;dbname=" . $this->db_name, $this->username, $this->password, $options);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
